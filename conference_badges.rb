cd959531b3b6cb1b8c30315a5440b4eb7e124d90
Alie Stone <helloaliestone@gmail.com>
2020-09-09 18:25:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def assign_rooms(guestrooms)
   array=[]
   names.each_with_index {|name,index|
   array.push ("Hello, #{name}! You'll be assigned to room #{index + 1}!")}
-  return array 
+  return array
 end
 
 
