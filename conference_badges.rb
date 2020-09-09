dbd5da4911a86bc898f169fd0e58a177b3c11aa8
Alie Stone <helloaliestone@gmail.com>
2020-09-09 18:14:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,12 +5,13 @@ def badge_maker(name)
 end
 
 def assign_rooms (name, room_assignments)
-  return "Hello, #{name}! You'll be assigned to room #{room_assignments}!"
+  new_array = []
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  names.each_with_index { |name, room_assignments| puts "Hello, #{names}! You'll be assigned to room #{room_assignments}!" }
 end
 
 
-
 def batch_badge_creator (array)
-  array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  return "Hello,#{array}! You'll be assigned to room #{room_assignments}!"
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  return "Hello,#{names}! You'll be assigned to room #{room_assignments}!"
 end
