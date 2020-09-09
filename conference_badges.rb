b2a26c88ff4873dba953d508a4465e705f41bb53
Alie Stone <helloaliestone@gmail.com>
2020-09-09 18:39:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,3 +19,12 @@ def batch_badge_creator(guests)
     badge_maker(name)
   end
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
