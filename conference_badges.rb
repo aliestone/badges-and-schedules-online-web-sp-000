5837d50620e3744a0358db96b936226bd3550065
Alie Stone <helloaliestone@gmail.com>
2020-09-09 18:27:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,12 +6,12 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def assign_rooms(guestrooms)
-  array=[]
-  names.each_with_index {|name,index|
-  array.push ("Hello, #{name}! You'll be assigned to room #{index + 1}!")
-}
-  return array
+def assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
 end
 
 
