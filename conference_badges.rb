a416a236a7b569b4736bcbc49c6a8b1944e5f8ab
Alie Stone <helloaliestone@gmail.com>
2020-09-09 18:20:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def assign_rooms(name,room_assignments)
+def assign_rooms(array)
   names.each_with_index { |name, index| puts "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
 end
 
